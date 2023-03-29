4f41b602f17507b3681f2a9c50e01e085794a712
Pavel Ekshin <pavel.ekshine@gmail.com>
2023-03-29 18:01:33 +0300

    PCC39 pavelekshin

--- 39/pavelekshin/test_app.py
+++ 39/pavelekshin/test_app.py
@@ -8,123 +8,110 @@ BAD_ITEM_URL = "{}/5".format(BASE_URL)
 GOOD_ITEM_URL = "{}/3".format(BASE_URL)
 
 
-def setUp():
-    backup_items = deepcopy(app.items)
-    app = app.test_client()
-    assert app.testing == False
+@pytest.fixture(scope="session")
+def create_app():
+    app
+    app.config.update(
+        {
+            "TESTING": True,
+        }
+    )
+    # other setup can go here
+    yield app
+    # clean up / reset resources here
 
 
-def test_get_items():
-    with app.test_client() as tc:
-        # get items
-        response = tc.get(BASE_URL)
-        assert response.status_code == 200
-        data = json.loads(response.data)
-        # items count in response
-        assert len(data["items"]) == 3
+@pytest.fixture()
+def client(create_app):
+    return app.test_client()
 
 
-def test_get_item():
-    with app.test_client() as tc:
-        # get one non exist item
-        response = tc.get(BAD_ITEM_URL)
-        assert response.status_code == 404
-        # get one exist item
-        response = tc.get(GOOD_ITEM_URL)
-        data = json.loads(response.data)
-        assert response.status_code == 200
-        assert len(data["items"]) == 1
+def test_get_items(client):
+    req = client.get(BASE_URL)
+    assert req.status_code == 200
+    data = json.loads(req.data)
+    assert len(data["items"]) == 3
 
 
-def test_not_found():
-    with app.test_client() as tc:
-        response = tc.get(BAD_ITEM_URL)
-        assert response.status_code == 404
+def test_get_item(client):
+    # get one non exist item
+    req = client.get(BAD_ITEM_URL)
+    assert req.status_code == 404
+    # get one exist item
+    req = client.get(GOOD_ITEM_URL)
+    data = json.loads(req.data)
+    assert req.status_code == 200
+    assert len(data["items"]) == 1
 
 
-def test_bad_request():
+def test_not_found(client):
+    req = client.get(BAD_ITEM_URL)
+    assert req.status_code == 404
+
+
+def test_bad_request(client):
     item = {"name": "laptop", "some-value": "1000"}
     # item wrong request body
-    with app.test_client() as tc:
-        response = tc.post(
-            BASE_URL, data=json.dumps(item), content_type="application/json"
-        )
-        assert response.status_code == 400
+    req = client.post(BASE_URL, data=json.dumps(item), content_type="application/json")
+    assert req.status_code == 400
 
 
-def test_create_item():
-    with app.test_client() as tc:
-        # wrong request body
-        item = {"name": "laptop", "some-value": "1000"}
-        response = tc.post(
-            BASE_URL, data=json.dumps(item), content_type="application/json"
-        )
-        assert response.status_code == 400
-        # item exist
-        item = {"name": "laptop", "value": int(1)}
-        response = tc.post(
-            BASE_URL, data=json.dumps(item), content_type="application/json"
-        )
-        assert response.status_code == 400
-        # item wrong request value type
-        item = {"name": "monitor", "value": str(1)}
-        response = tc.post(
-            BASE_URL, data=json.dumps(item), content_type="application/json"
-        )
-        assert response.status_code == 400
-        # create item
-        item = {"name": "printer", "value": 100}
-        reponse = tc.post(
-            BASE_URL, data=json.dumps(item), content_type="application/json"
-        )
+def test_create_item(client):
+    # wrong request body
+    item = {"name": "laptop", "some-value": "1000"}
+    req = client.post(BASE_URL, data=json.dumps(item), content_type="application/json")
+    assert req.status_code == 400
+    # item exist
+    item = {"name": "laptop", "value": int(1)}
+    req = client.post(BASE_URL, data=json.dumps(item), content_type="application/json")
+    assert req.status_code == 400
+    # item wrong request value type
+    item = {"name": "monitor", "value": str(1)}
+    req = client.post(BASE_URL, data=json.dumps(item), content_type="application/json")
+    assert req.status_code == 400
+    # create item
+    item = {"name": "printer", "value": 100}
+    reponse = client.post(
+        BASE_URL, data=json.dumps(item), content_type="application/json"
+    )
 
 
-def test_update_item():
+def test_update_item(client):
     item = {"name": "laptop", "some-value": "1000"}
     # item wrong request body
-    with app.test_client() as tc:
-        response = tc.post(
-            BASE_URL, data=json.dumps(item), content_type="application/json"
-        )
-        assert response.status_code == 400
+    req = client.post(BASE_URL, data=json.dumps(item), content_type="application/json")
+    assert req.status_code == 400
 
 
-def test_update_item():
-    with app.test_client() as tc:
-        # update non exist item
-        response = tc.put(BAD_ITEM_URL)
-        assert response.status_code == 404
-        # empty request body
-        response = tc.put(
-            GOOD_ITEM_URL, data=json.dumps({}), content_type="application/json"
-        )
-        assert response.status_code == 400
-        # request body contains str instead of int value
-        response = tc.put(
-            GOOD_ITEM_URL,
-            data=json.dumps({"name": "book", "value": str(10)}),
-            content_type="application/json",
-        )
-        assert response.status_code == 400
-        # correct request
-        response = tc.put(
-            GOOD_ITEM_URL,
-            data=json.dumps({"name": "book", "value": int(10)}),
-            content_type="application/json",
-        )
-        assert response.status_code == 200
+def test_update_item(client):
+    # update non exist item
+    req = client.put(BAD_ITEM_URL)
+    assert req.status_code == 404
+    # empty request body
+    req = client.put(
+        GOOD_ITEM_URL, data=json.dumps({}), content_type="application/json"
+    )
+    assert req.status_code == 400
+    # request body contains str instead of int value
+    req = client.put(
+        GOOD_ITEM_URL,
+        data=json.dumps({"name": "book", "value": str(10)}),
+        content_type="application/json",
+    )
+    assert req.status_code == 400
+    # correct request
+    req = client.put(
+        GOOD_ITEM_URL,
+        data=json.dumps({"name": "book", "value": int(10)}),
+        content_type="application/json",
+    )
+    assert req.status_code == 200
 
 
-def test_delete_item():
-    with app.test_client() as tc:
-        # delete non exist item
-        response = tc.delete(BAD_ITEM_URL)
-        assert response.status_code == 404
-        # delete exist item
-        response = tc.delete(GOOD_ITEM_URL)
-        assert response.status_code == 204
-
-
-def setDown():
-    # reset state
-    app.items = backup_items
+def test_delete_item(client):
+    # delete non exist item
+    req = client.delete(BAD_ITEM_URL)
+    assert req.status_code == 404
+    # delete exist item
+    req = client.delete(GOOD_ITEM_URL)
+    assert req.status_code == 204
